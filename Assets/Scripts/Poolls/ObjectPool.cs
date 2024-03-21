b6bbd5269e8a362e704baf1bcac170365ec65102
Artem <artem15stepanenko@outlook.com>
2024-03-21 19:23:25 +0300

    Attack system and object pool

---
+++ Assets/Scripts/Poolls/ObjectPool.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T>
+{
+    private Queue<T> _objectsQueue;
+
+    private Func<T> _preloadFunc;
+    private Action<T> _getAction;
+    private Action<T> _returnAction;
+
+    public ObjectPool(int objectsStartCount, Func<T> preloadFunc, Action<T> getAction, Action<T> returnAction)
+    {
+        _objectsQueue = new Queue<T>();
+
+        _preloadFunc = preloadFunc;
+        _getAction = getAction;
+        _returnAction = returnAction;
+
+        for (int i = 0; i < objectsStartCount; i++)
+        {
+            _objectsQueue.Enqueue(_preloadFunc());
+        }
+    }
+
+    public T Get()
+    {
+        T item = _objectsQueue.Count > 0 ? _objectsQueue.Dequeue() : _preloadFunc();
+
+        _getAction(item);
+
+        return item;
+    }
+
+    public void Return(T item) 
+    {
+        _returnAction(item);
+        _objectsQueue.Enqueue(item);
+    }
+}
