65d5c927a8bc8ce9c42b05c74a80f011d51088ec
Boomrock <gorlopan.03@gmail.com>
2024-04-16 15:03:38 +0700

    Add spawn point

--- Assets/Scripts/Generator/WallGenerator.cs
+++ Assets/Scripts/Generator/WallGenerator.cs
@@ -6,18 +6,18 @@ public class WallGenerator
     public static void SetWall(TerrainType[,] map)
     {
         var copyMap = map;
-
-        for (int x = 0; x < map.GetLength(0); x++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            for (int y = 0; y < map.GetLength(1); y++)
+            for (int x = 0; x < map.GetLength(1); x++)
             {
+ 
                 bool needWall = false;
                 MatrixTools.SpiralArrayTraversal(map,new Vector2Int(x,y), i =>
                 {
-                    if(i.x == x && i.y == y && map[i.x, i.y] != TerrainType.None) return MatrixTools.Result.Break;
+                    if(i.x == x && i.y == y && map[i.y, i.x] != TerrainType.None) return MatrixTools.Result.Break;
                     if(i.x == x && i.y == y) return MatrixTools.Result.None;
 
-                    if (map[i.x, i.y] == TerrainType.Floor)
+                    if (map[i.y, i.x] == TerrainType.Floor)
                     {
                         needWall = true;
                         return MatrixTools.Result.Break;
@@ -25,7 +25,7 @@ public class WallGenerator
                     
                     return MatrixTools.Result.None;
                 }, 1);
-                if (needWall) map[x, y] = TerrainType.Wall;
+                if (needWall) map[y, x] = TerrainType.Wall;
             }
         }
     }
